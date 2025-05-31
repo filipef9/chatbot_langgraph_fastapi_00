806918428ef536cf7a72667e12f9eaac10cdd407
Filipe dos Santos Nascimento <filipef9@hotmail.com>
2025-05-31 20:25:25 +0000

    :sparkles: Configuração do modelo LLM e do template do prompt system.

---
+++ llm.py
@@ -0,0 +1,13 @@
+from langchain_groq import ChatGroq
+from langchain import ChatPromptTemplate, MessagesPlaceholder
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+prompt_template = ChatPromptTemplate.from_messages(
+  [
+    ("system", "{system_message}"),
+    MessagesPlaceholder("messages"),
+  ]
+)
+
+chain = prompt_template | llm
